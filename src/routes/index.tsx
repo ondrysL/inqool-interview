926d658f0acebccfab49c7c9f81595338a3c2ec3
Ondrej Linart <linart.ondrej@gmail.com>
2025-06-20 19:31:38 +0200

    feat: install all library, add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,30 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {
+    Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
     <div className="p-2">
       <h3 className="">Welcome Home!</h3>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
-  )
+  );
 }
