f10dfbe1a3ac6a744dd0c4481aec1823d55871e7
Ondrej Linart <linart.ondrej@gmail.com>
2025-06-21 16:10:11 +0200

    feat: add hook form dev tools, test api

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
-import { useCreateUser, useDeleteUser, UserSchema } from "@/features/users";
+import {
+  useCreateUser,
+  useDeleteUser,
+  UserSchema,
+  useUpdateUser,
+} from "@/features/users";
 import { useUsers } from "@/features/users/hooks/useUsers";
 import { createFileRoute } from "@tanstack/react-router";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, useForm, useWatch } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Select,
@@ -11,10 +16,69 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox";
+import { DevTool } from "@hookform/devtools";
+import { useEffect, useState } from "react";
+import type { User } from "@/api/generated";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
+
+const UserUpdate = ({
+  user,
+  resetId,
+}: {
+  user: User;
+  resetId: (id: null) => void;
+}) => {
+  const { handleSubmit, control, register } = useForm({
+    resolver: zodResolver(UserSchema),
+    defaultValues: {
+      name: user.name,
+      gender: user.gender,
+      banned: user.banned,
+    },
+  });
+
+  const { mutateAsync: updateUser } = useUpdateUser(user.id);
+
+  return (
+    <form
+      className="flex"
+      onSubmit={handleSubmit(async (data) => {
+        updateUser(data);
+        resetId(null);
+      })}
+    >
+      <input type="text" {...register("name")} />
+      <Controller
+        control={control}
+        name="gender"
+        render={({ field }) => (
+          <Select value={field.value} onValueChange={field.onChange}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Theme" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="male">Male</SelectItem>
+              <SelectItem value="female">Female</SelectItem>
+              <SelectItem value="other">Other</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="banned"
+        render={({ field }) => (
+          <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+        )}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 function Index() {
   const { data: users } = useUsers();
@@ -25,21 +89,33 @@ function Index() {
     resolver: zodResolver(UserSchema),
   });
 
+  const [updateId, setUpdateId] = useState<string | null>(null);
+
   return (
     <div className="p-2">
-      {users.map((user) => (
-        <div key={user.id} className="flex gap-x-2">
-          <h1>{user.name}</h1>
-          <p>{user.banned ? "banned" : "not banned"}</p>
-          <p>{user.gender}</p>
-          <button
-            className="bg-red-200 p-2 cursor-pointer"
-            onClick={() => mutateAsync(user.id)}
-          >
-            Delete
-          </button>
-        </div>
-      ))}
+      {users.map((user) =>
+        updateId === user.id ? (
+          <UserUpdate key={user.id} user={user} resetId={setUpdateId} />
+        ) : (
+          <div key={user.id} className="flex gap-x-2">
+            <h1>{user.name}</h1>
+            <p>{user.banned ? "banned" : "not banned"}</p>
+            <p>{user.gender}</p>
+            <button
+              className="bg-red-200 p-2 cursor-pointer"
+              onClick={() => mutateAsync(user.id)}
+            >
+              Delete
+            </button>
+            <button
+              className="bg-amber-200 p-2 cursor-pointer"
+              onClick={() => setUpdateId(user.id)}
+            >
+              Edit
+            </button>
+          </div>
+        ),
+      )}
       <h1>{users.length}</h1>
       <form
         className="mb-12"
@@ -79,6 +155,7 @@ function Index() {
         {formState.isSubmitting && <p>Loading...</p>}
         {formState.errors.banned && <p>{formState.errors.banned.message}</p>}
       </form>
+      <DevTool control={control} />
     </div>
   );
 }
